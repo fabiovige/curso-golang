ac2e3a940089df5bbcd38d2e2395049bd24abe44
Fabio Vige <fabiovige@gmail.com>
2024-02-27 20:58:38 -0300

    secao 1 > fundamentos > variáveis

---
+++ secao1-fundamentos/variaveis.go
@@ -0,0 +1,16 @@
+package variaveis
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+	var mensagem string
+	mensagem = "Funcionalidades"
+	fmt.Println(name, age, mensagem)
+
+	nova_mensagem := "Novamensagem"
+	nova_idade := 10
+	preco := 20.5
+	fmt.Println(nova_mensagem, nova_idade, preco)
+}
